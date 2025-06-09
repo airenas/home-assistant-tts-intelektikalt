07f915a7e79d3ed1d4a285dcba9378b392a81de6
Airenas Vaiciunas <airenass@gmail.com>
2025-06-10 00:16:39 +0300

    refactor: clean code

--- custom_components/intelektikalt_tts/tts.py
+++ custom_components/intelektikalt_tts/tts.py
@@ -2,14 +2,14 @@ import base64
 import logging
 
 import async_timeout
-from custom_components.homeassistant_tts_intelektikalt.const import CONF_VOICE, API_URL, API_TITLE, API_LANGUAGE, \
-    API_FORMAT, VoiceEnum
 from homeassistant.components.tts import Provider, TextToSpeechEntity, Voice
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.typing import ConfigType
+
+from custom_components.intelektikalt_tts.const import CONF_VOICE, API_URL, API_TITLE, API_LANGUAGE, \
+    API_FORMAT, VoiceEnum
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,83 +19,8 @@ async def async_get_engine(hass, config, discovery_info=None):
     return IntelektikaLTTTSProvider(hass, config)
 
 
-async def async_setup_entry(
-        hass: HomeAssistant,
-        config_entry: ConfigEntry,
-        async_add_entities,
-) -> None:
-    async_add_entities([IntelektikaLTTTSEntity(hass, config_entry.data)])
-
-
-class IntelektikaLTTTSEntity(TextToSpeechEntity):
-    def __init__(self, hass: HomeAssistant, config_entry: ConfigType) -> None:
-        _LOGGER.info(f"Init {__name__}")
-        self._hass = hass
-        self._language = API_LANGUAGE
-        self._api_key = config_entry.get("api_key")
-        self._voice = VoiceEnum.default().value
-        _LOGGER.info(f"Using voice: {self._voice}")
-        self._url = API_URL
-
-    @property
-    def default_language(self) -> str:
-        """Return the default language."""
-        return self._language
-
-    @property
-    def supported_languages(self) -> list[str]:
-        """Return list of supported languages."""
-        return [self._language]
-
-    @property
-    def name(self):
-        return API_TITLE
-
-    @property
-    def supported_options(self) -> list[str]:
-        """Return a list of supported options."""
-        return [CONF_VOICE]
-
-    @callback
-    def async_get_supported_voices(self, language: str) -> list[Voice] | None:
-        """Return a list of supported voices for a language."""
-        if language == self._language:  # Assuming `self._language` is the default language
-            return [
-                Voice(voice_id=VoiceEnum.LAIMIS.value, name="Laimis"),
-                Voice(voice_id=VoiceEnum.LINA.value, name="Lina"),
-                Voice(voice_id=VoiceEnum.ASTRA.value, name="Astra"),
-                Voice(voice_id=VoiceEnum.VYTAUTAS.value, name="Vytautas"),
-            ]
-        return None
-
-    async def async_get_tts_audio(self, message, language, options=None):
-        try:
-            selected_voice = options.get(CONF_VOICE) if options else None
-            if not selected_voice:
-                selected_voice = self._voice
-
-            payload = {"text": message,
-                       "outputFormat": API_FORMAT,
-                       "outputTextFormat": "none",
-                       "saveRequest": False,
-                       "speed": 1,
-                       "voice": selected_voice
-                       }
-            headers = {"Authorization": f"Key {self._api_key}"} if self._api_key else {}
-
-            _LOGGER.info("TTS request: %s", payload)
-
-            async with async_timeout.timeout(10):
-                session = async_get_clientsession(self._hass)
-                async with session.post(self._url, json=payload, headers=headers) as resp:
-                    resp.raise_for_status()
-                    data = await resp.json()
-                    audio_bytes = base64.b64decode(data.get("audioAsString"))
-                    return API_FORMAT, audio_bytes
-
-        except Exception as e:
-            _LOGGER.error("Error fetching TTS audio: %s", e)
-            return None, None
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities) -> None:
+    async_add_entities([IntelektikaLTTTSEntity(provider=IntelektikaLTTTSProvider(hass, config_entry.data))])
 
 
 class IntelektikaLTTTSProvider(Provider):
@@ -109,6 +34,10 @@ class IntelektikaLTTTSProvider(Provider):
 
     @property
     def default_language(self):
+        return self._language
+
+    @property
+    def language(self):
         return self._language
 
     @property
@@ -151,3 +80,43 @@ class IntelektikaLTTTSProvider(Provider):
     @property
     def supports_streaming(self) -> bool:
         return False
+
+
+class IntelektikaLTTTSEntity(TextToSpeechEntity):
+    def __init__(self, provider: IntelektikaLTTTSProvider) -> None:
+        _LOGGER.info(f"Init {__name__}")
+        self._provider = provider
+
+    @property
+    def default_language(self) -> str:
+        """Return the default language."""
+        return self._provider.default_language
+
+    @property
+    def supported_languages(self) -> list[str]:
+        """Return list of supported languages."""
+        return self._provider.supported_languages
+
+    @property
+    def name(self):
+        return self._provider.name
+
+    @property
+    def supported_options(self) -> list[str]:
+        """Return a list of supported options."""
+        return [CONF_VOICE]
+
+    @callback
+    def async_get_supported_voices(self, language: str) -> list[Voice] | None:
+        """Return a list of supported voices for a language."""
+        if language == self._provider.language:
+            return [
+                Voice(voice_id=VoiceEnum.LAIMIS.value, name="Laimis"),
+                Voice(voice_id=VoiceEnum.LINA.value, name="Lina"),
+                Voice(voice_id=VoiceEnum.ASTRA.value, name="Astra"),
+                Voice(voice_id=VoiceEnum.VYTAUTAS.value, name="Vytautas"),
+            ]
+        return None
+
+    async def async_get_tts_audio(self, message, language, options=None):
+        return self._provider.async_get_supported_voices(message=message, language=language, options=options)
