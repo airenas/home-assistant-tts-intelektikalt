092d3dfe25064fc76299ff2dbd5ba18f4138c4d0
Airenas Vaiciunas <airenass@gmail.com>
2025-06-09 17:03:55 +0300

    refactor: enhance TTS provider with async support and update manifest structure

--- custom_components/homeassistant_tts_intelektikalt/__init__.py
+++ custom_components/homeassistant_tts_intelektikalt/__init__.py
@@ -10,12 +10,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 
 from .const import DOMAIN, LOGGER
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
 
+PLATFORMS: list[Platform] = [Platform.TTS]
 
 # https://developers.home-assistant.io/docs/config_entries_index/#setting-up-an-entry
 async def async_setup_entry(
--- custom_components/homeassistant_tts_intelektikalt/tts.py
+++ custom_components/homeassistant_tts_intelektikalt/tts.py
@@ -1,14 +1,20 @@
 import logging
-import requests
+
+import async_timeout
 from homeassistant.components.tts import Provider
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_get_engine(hass, config, discovery_info=None):
-    return IntelektikaLTTTSProvider(config)
+    """Return the TTS provider instance."""
+    return IntelektikaLTTTSProvider(hass, config)
+
 
 class IntelektikaLTTTSProvider(Provider):
-    def __init__(self, config):
+    def __init__(self, hass, config):
+        self._hass = hass
         self._language = "lt"
         self._api_key = config.get("api_key")
         self._voice = config.get("voice", "laimis")
@@ -22,26 +28,28 @@ class IntelektikaLTTTSProvider(Provider):
     def supported_languages(self):
         return [self._language]
 
-    def get_tts_audio(self, message, language, options=None):
+    async def async_get_tts_audio(self, message, language, options=None):
         try:
-            payload = {"text": message, 
-                "outputFormat": "wav", 
-                "outputTextFormat": "none", 
-                "saveRequest": False,
-                "speed": 1, 
-                "voice":self._voice
-            }
+            payload = {"text": message,
+                       "outputFormat": "wav",
+                       "outputTextFormat": "none",
+                       "saveRequest": False,
+                       "speed": 1,
+                       "voice": self._voice
+                       }
             headers = {"Authorization": f"Key {self._api_key}"} if self._api_key else {}
 
-            response = requests.post(
-                self._url,
-                json=payload,
-                headers=headers,
-                timeout=10
-            )
-            response.raise_for_status()
-            return ("audio/wav", response.content)
+            async with async_timeout.timeout(10):
+                session = async_get_clientsession(self._hass)
+                async with session.post(self._url, json=payload, headers=headers) as resp:
+                    resp.raise_for_status()
+                    data = await resp.read()
+                    return ("audio/wav", data)
 
         except Exception as e:
             _LOGGER.error("Error fetching TTS audio: %s", e)
             return (None, None)
+
+    @property
+    def supports_streaming(self) -> bool:
+        return False
